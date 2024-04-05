d990962f71a9c4c375c40373bfe5eacaf413ee15
Simon Thornley <sithor@gmail.com>
2024-04-05 23:10:37 +1300

    setup file

--- setup.R
+++ setup.R
@@ -1,2 +1,9 @@
-pacman::p_load(vioplot, sm, beanplot, survival, graphics, stats)
+if(!require(pacman)) install.packages("pacman")
+pacman::p_load(vioplot, sm, beanplot, survival, graphics, stats, usethis)
 #install.packages
+library(usethis)
+usethis::use_git_config(user.name = "Simon Thornley", 
+               user.email = "sithor@gmail.com")
+
+## use this as password through Rstudio
+usethis::create_github_token()
